b5e64bafca35c7549984029e4995d61c6d475309
Felix von Leitner <fefe@gmail.com>
2013-12-29 03:11:53 +0100

    Distributed Server Factory. More decentralisation!

---
+++ de/fefe/dietjabberd/DietJabberdDistributedServerManagerFactory.java
@@ -0,0 +1,6 @@
+public class DietJabberdDistributedServerManagerFactory {
+    public static void main(String[] args) {
+        System.out.println("This jabberd has currently no known bugs!\n");
+    }
+}
+
