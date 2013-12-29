c0a213f99c5ec576defa9b091790c3cfeb2d049d
Stefan Haun <tux@antiblau.de>
2013-12-29 10:49:59 +0100

    Added the package declaration for de.fefe.dietjabberd

--- de/fefe/dietjabberd/DietJabberdDistributedServerManagerFactory.java
+++ de/fefe/dietjabberd/DietJabberdDistributedServerManagerFactory.java
@@ -1,3 +1,5 @@
+package de.fefe.dietjabberd;
+
 public class DietJabberdDistributedServerManagerFactory {
     public static void main(String[] args) {
         System.out.println("This jabberd has currently no known bugs!\n");
